c762076b30b99a26df057de132462186ddee5af2
Pedro Henrique <contatophss11@gmail.com>
2022-08-23 12:21:23 -0300

    adicionando about, e projetos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 
 
+
+
+
+// Função para abrir GitHub e Linkedin em Aba Separada
+const gitButton = document.getElementById('git-button')
+const linkedinButton = document.getElementById('linkedin-button')
+
+
+gitButton.addEventListener("click", abreGit)
+linkedinButton.addEventListener("click", abreLinkedin)
+
+function abreGit() {
+    window.open("https://github.com/PedroSS11")
+
+}
+
+function abreLinkedin() {
+    window.open("https://www.linkedin.com/in/pedrohenriquedev/")
+}
+
+
+
+// Teste Dark / Light Mode
+
 const body = document.getElementsByTagName('body')
+
